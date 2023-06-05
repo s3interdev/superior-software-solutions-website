bcb92acc4bcf51e2f54ef37d0f8bf2a6002c45d8
Michael Owuor <michael.owuor@gmail.com>
2023-06-05 21:09:48 +0300

    fix: create hero component

--- app/components/index.js
+++ app/components/index.js
@@ -1,4 +1,5 @@
 export { default as DisplayContent } from './display-content';
+export { default as DisplayHeroStandard } from './display-hero-standard';
 export { default as DisplayImage } from './display-image';
 export { default as NavFooter } from './nav-footer';
 export { default as NavHeader } from './nav-header';
